d6dad6f48227fc1df86d6b3abe74cd48d17da7f9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-11-26 16:21:56 -0300

    fix: adjust build entrypoint
    - stop minify and compile in strict mode

--- bun-build.config.js
+++ bun-build.config.js
@@ -1,12 +1,7 @@
 await Bun.build({
   entrypoints: ['./src/index.ts'],
-  outdir: './dist',
+  outdir: './dist/src',
   sourcemap: 'external',
   target: 'node',
   external: ['openai'],
-  minify: {
-    identifiers: true,
-    syntax: true,
-    whitespace: false,
-  }
 });
